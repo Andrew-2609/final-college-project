3167f98c55fdc6e3bf854b372486927e33e57829
Andrew Monteiro <andrew.agoravai@gmail.com>
2025-05-13 22:38:27 -0300

    chore: add error log for Repository errors

--- src/infrastructure/repositories/error.rs
+++ src/infrastructure/repositories/error.rs
@@ -1,7 +1,10 @@
+use log::error;
+
 use crate::domain::errors::repository_error::RepositoryError;
 
 impl From<diesel::result::Error> for RepositoryError {
     fn from(value: diesel::result::Error) -> Self {
+        error!("Database error: {}", value);
         RepositoryError::DatabaseError(value.to_string())
     }
 }
